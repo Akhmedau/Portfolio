44a9f42b4cf73ac665812d97459b1ede7aa73dd2
github-username <akhmiedov.199629@gmail.com>
2022-10-25 14:26:23 +0200

    Initial Commit

---
+++ SimpleNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleNotes
+//
+//  Created by АХМЕДОВ НИКОЛАЙ on 25/10/2022.
+//  Copyright © 2022 Ahmedov Nikolay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
